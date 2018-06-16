77ca766338d6b131f4db0f947bc3b6b8803cd2ba
mateusgithu <39654929+mateusgithu@users.noreply.github.com>
2018-06-16 17:33:32 -0300

    Classes,Funções,Metodos em Python

---
+++ aluno.py
@@ -0,0 +1,8 @@
+class Aluno():
+    nome="Mateus"
+    _idade=17
+    endereco="Teresina-PI"
+    
+    def set_idade(set,idade):
+        if idade>0:
+            self._idade=idade
---
+++ new_aluno.py
@@ -0,0 +1,8 @@
+from aluno import Aluno
+
+aluno1 = Aluno()
+
+print("Nome", aluno1.nome)
+print("Idade",aluno1._idade)
+print("Endereço",aluno1.endereco)
+   
